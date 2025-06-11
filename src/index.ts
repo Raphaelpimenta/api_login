169b8e7e409f154aae561163ca7c8525fc6b270c
Raphaelpimenta <raphael.pimentagomes@hotmail.com>
2025-06-11 18:52:33 -0300

    Teste em rotas com express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
+import express, { Request, Response} from 'express'
 
-console.log('Hello world ATUALIZADO')
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (request: Request, response: Response ): any => {
+    return response.status(200).json({ message: "My api"})
+})
+
+server.post('/user', (request: Request, response: Response): any => {
+    const body = request.body
+    console.log(body)
+    return response.status(201).json({ message: "Usuário criado"})
+})
+
+server.listen(5000, () => {console.log('server on update')})
